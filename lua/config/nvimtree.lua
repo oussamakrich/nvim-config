d39f0c32dc41066a491b15fcdf578e240ea9f289
lf9ih <lf9ih@lf9ihs-MacBook-Pro.local>
2023-04-19 14:52:41 +0000

    chore: tweak neotree

--- lua/config/nvimtree.lua
+++ lua/config/nvimtree.lua
@@ -1,3 +1,27 @@
-require'neo-tree'.setup { }
+vim.cmd [[ let g:neo_tree_remove_legacy_commands = 1 ]]
 
-require("keymaps").nvim_tree()
+require('neo-tree').setup {
+  enable_diagnostics = false,
+  default_component_configs = {
+    name = {
+      use_git_status_colors = false,
+    },
+    git_status = {
+      symbols = {
+        -- Change type
+        added = '[+]',    -- or "✚", but this is redundant info if you use git_status_colors on the name
+        modified = '[]', -- or "", but this is redundant info if you use git_status_colors on the name
+        deleted = '[✖]', -- this can only be used in the git_status source
+        renamed = '[]', -- this can only be used in the git_status source
+        -- Status type
+        untracked = '[?]',
+        ignored = '[]',
+        unstaged = '[-]',
+        staged = '[]',
+        conflict = '[]',
+      },
+    },
+  },
+}
+
+require('keymaps').nvim_tree()
