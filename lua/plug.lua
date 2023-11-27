b3e15cf0f8815b77307694b8cd3a2318404b20c6
oussama <sakawioussama@gmail.com>
2023-11-27 16:42:51 +0000

    chng blankline

--- lua/plug.lua
+++ lua/plug.lua
@@ -88,15 +88,10 @@ require('lazy').setup({
 
   {
     -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
+    'lukas-reineke/indent-blankline.nvim', main = 'ibl', 
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help indent_blankline.txt`
-    opts = {
-      char = '┊',
-      indentLine_enabled = 1,
-      show_first_indent_level = false,
-      show_trailing_blankline_indent = false,
-    },
+    opts = {},
   },
 
   -- "gc" to comment visual regions/lines
